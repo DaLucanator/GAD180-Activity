230a34704de3de5c2c5d3471e580b01be5ff3420
Martin <1021419@student.sae.edu.au>
2021-03-16 16:45:39 +0800

    Add FlappyUgg

---
+++ Assets/Scripts/FlappyUgg/DeathClubFlappyUgg.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathClubFlappyUgg : MonoBehaviour
+{
+   public Vector2 SpawnPoint;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D (Collider2D col)
+     {
+         if     (col.tag == "Player")
+         {
+             col.transform.position = SpawnPoint;
+         }
+     }
+}
---
+++ Assets/Scripts/FlappyUgg/FlappyUggPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyUggPlayer : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float jumpForce;
+    public float moveSpeed;
+    Vector2 moveForward;
+    bool canGoForward = true;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        Jump();
+
+        if (canGoForward) 
+        { 
+             moveForward = gameObject.transform.position;
+             moveForward.x += 0.1f * moveSpeed;
+             gameObject.transform.position = moveForward;  
+        }
+    }
+
+    
+
+    void Jump() 
+    { 
+        if (Input.GetKeyDown(KeyCode.Space)) 
+       { 
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce); 
+       } 
+    }
+
+}
