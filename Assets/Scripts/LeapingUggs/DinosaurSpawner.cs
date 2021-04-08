d8e307a6b2b6be9ee865f7fecb927250f6b0875a
Martin <1021419@student.sae.edu.au>
2021-04-08 21:23:36 +0800

    Added game 'LeapingUgg'
    It's basically Frogger but with more Ugg

---
+++ Assets/Scripts/LeapingUggs/DinosaurSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class DinosaurSpawner : MonoBehaviour
+{
+
+      public float spawnDelay = 3f;
+
+      public GameObject Dinosaur;
+
+      public Transform[] spawnPoints;
+
+      public float nextTimeToSpawn = 0f;
+
+    void Update ()
+    {
+        if (nextTimeToSpawn <= Time.time)
+        {
+             SpawnDinosaur();
+             nextTimeToSpawn = Time.time + spawnDelay;
+        }
+    }  
+
+   
+    void SpawnDinosaur ()
+    {
+         int randomIndex = Random.Range(0, spawnPoints.Length);
+         Transform spawnPoint = spawnPoints[randomIndex];
+
+         Instantiate(Dinosaur, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
