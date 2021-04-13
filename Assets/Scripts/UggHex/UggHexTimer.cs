75a75f88678d8a49e528d188e56897febae36e2d
Martin <1021419@student.sae.edu.au>
2021-04-13 16:54:37 +0800

    Added game 'UggHex'

---
+++ Assets/Scripts/UggHex/UggHexTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UggHexTimer : MonoBehaviour
+{
+
+     float TmStart;
+     float TmLen=10f;
+ 
+     // Use this for initialization
+     void Start () {
+         TmStart=Time.time;
+     }
+     
+     // Update is called once per frame
+     void Update () {
+         if(Time.time>TmStart + TmLen) 
+         {
+ 
+             Debug.Log("You Win");
+         }
+     }
+}
---
+++ Assets/Scripts/UggHex/UggHex_Rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UggHex_Rotator : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime * 30f);
+    }
+}
